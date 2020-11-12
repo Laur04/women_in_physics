49a94ade8ef7f2ff66695a632aecef57bec5dfa7
lauren <pmd7211@gmail.com>
2020-11-12 10:51:51 -0500

    fix url paths

--- women_in_physics/apps/classroom/urls.py
+++ women_in_physics/apps/classroom/urls.py
@@ -1,13 +1,13 @@
-from django.conf.urls import url
+from django.urls import path
 
 from . import views
 
 app_name = 'classroom'
 
 urlpatterns = [
-    url(r'^create/', views.create, name='create'),
-    url(r'^classroom/$', views.classroom, name='classroom'),
-    url(r'^response/$', views.response, name='response'),
-    url(r'^changestatus/(?P<post>\w+)/$', views.changestatus, name='changestatus'),
-    url(r'^reset/student/$', views.reset_student, name='reset_student'),
+    path('create/', views.create, name='create'),
+    path('classroom/', views.classroom, name='classroom'),
+    path('response/', views.response, name='response'),
+    path('changestatus/<slug:post>/', views.changestatus, name='changestatus'),
+    path('reset/student/', views.reset_student, name='reset_student'),
 ]
